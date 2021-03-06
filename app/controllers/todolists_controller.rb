d6b41f7029a24f0a48680be6305b57b00cd98ca0
EC2 Default User <ec2-user@ip-172-31-31-16.ec2.internal>
2021-03-06 11:14:05 +0000

    start[2-5]

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -3,15 +3,25 @@ class TodolistsController < ApplicationController
     @list = List.new
   end
   
+  
+  
   def create
+   
     list = List.new(list_params)
+    
     list.save
-    redirect_to "/top"
+    
+    redirect_to '/top'
   end
   
-  praivate
+  def index
+    @lists = List.all 
+  end
+
+  private
+  
   def list_params
     params.require(:list).permit(:title, :body)
   end
   
-end
+end  
